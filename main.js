afd95896fdfc5eb68e19173dd054aef3404d2761
ahasannahid <ahasan15-9072@diu.edu.bd>
2023-08-19 14:47:05 +0600

    fs module

--- main.js
+++ main.js
@@ -1,36 +1,35 @@
-const { write } = require('fs');
 const http = require('http');
 const URL = require('url');
 
 const server = http.createServer(function(req,res){
     // res.end("Hello world")
-    // if(req.url =='/'){
-    //     res.writeHead(200,{'Content-type': 'text/html'})
-    //     res.write('<h1>This is Home page</h1>')
-    //     res.end();
-    // }
-    // else if(req.url =='/about'){
-    //     res.writeHead(200,{'Content-type': 'text/html'})
-    //     res.write('<h1>This is About page</h1>')
-    //     res.end();
-    // }
-    // else if(req.url =='/contact'){
-    //     res.writeHead(200,{'Content-type': 'text/html'})
-    //     res.write('<h1>This is Contact page</h1>')
-    //     res.end();
-    // }
+    if(req.url =='/'){
+        res.writeHead(200,{'Content-type': 'text/html'})
+        res.write('<h1>This is Home page</h1>')
+        res.end();
+    }
+    else if(req.url =='/about'){
+        res.writeHead(200,{'Content-type': 'text/html'})
+        res.write('<h1>This is About page</h1>')
+        res.end();
+    }
+    else if(req.url =='/contact'){
+        res.writeHead(200,{'Content-type': 'text/html'})
+        res.write('<h1>This is Contact page</h1>')
+        res.end();
+    }
 
-    const myURL = 'https://unique-recipe.web.app/blog';
+    // const myURL = 'https://unique-recipe.web.app/blog';
 
-    var myURLObj = URL.parse(myURL,true);
+    // var myURLObj = URL.parse(myURL,true);
 
-    var myHostname =  myURLObj.host;
-    var myPathname =  myURLObj.pathname;
+    // var myHostname =  myURLObj.host;
+    // var myPathname =  myURLObj.pathname;
 
-    res.writeHead(210,{'Content-type': 'text/html'});
-    res.write(myHostname);
-    res.write(myPathname);
-    res.end();
+    // res.writeHead(210,{'Content-type': 'text/html'});
+    // res.write(myHostname);
+    // res.write(myPathname);
+    // res.end();
     
 });
 
